089b1c871faf8af6f70ce2aedb09588cedc14956
Mékéné <64429720+IsmaelMekene@users.noreply.github.com>
2021-03-23 05:04:59 +0100

    Update and rename models.py to baseline_model.py

--- model/baseline_model.py
+++ model/baseline_model.py
@@ -1,3 +1,7 @@
+''' 
+@uthor: metero_21
+'''
+
 import os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,6 +15,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 
+#Build the baseline model
 
 class CTCLayer(layers.Layer):
     def __init__(self, name=None):
@@ -99,6 +104,8 @@ model.summary()
 
 
 
+#Train the baseline model
+
 logdir = "logs/baseline_model/" + datetime.now().strftime("%Y%m%d-%H%M%S")
 epochs = 100
 early_stopping_patience = 10
@@ -128,4 +135,53 @@ history = model.fit(
 
 
 print('The average test loss is: ', np.average(history.history['loss']))
+
+
+
+#Test inference on the baseline model
+
+# Get the prediction model by extracting layers till the output layer
+baseline_prediction_model = keras.models.Model(
+    model.get_layer(name="image").input, model.get_layer(name="dense2").output
+)
+baseline_prediction_model.summary()
+
+# A utility function to decode the output of the network
+def decode_batch_predictions(pred):
+    input_len = np.ones(pred.shape[0]) * pred.shape[1]
+    # Use greedy search. For complex tasks, you can use beam search
+    results = keras.backend.ctc_decode(pred, input_length=input_len, greedy=True)[0][0][
+        :, :max_length
+    ]
+    # Iterate over the results and get back the text
+    output_text = []
+    for res in results:
+        res = tf.strings.reduce_join(num_to_char(res)).numpy().decode("utf-8")
+        output_text.append(res)
+    return output_text
+
+
+#  Let's check results on some validation samples
+for batch in validation_dataset.take(1):
+    batch_images = batch["image"]
+    batch_labels = batch["label"]
+
+    preds = baseline_prediction_model.predict(batch_images)
+    pred_texts = decode_batch_predictions(preds)
+    '''
+    orig_texts = []
+    for label in batch_labels:
+        label = tf.strings.reduce_join(num_to_char(label)).numpy().decode("utf-8")
+        orig_texts.append(label)
+    '''
+    _, ax = plt.subplots(4, 4, figsize=(15, 5))
+    for i in range(len(pred_texts)):
+        img = (batch_images[i, :, :, 0] * 255).numpy().astype(np.uint8)
+        img = img.T
+        title = f"Prediction: {pred_texts[i]}"
+        ax[i // 4, i % 4].imshow(img, cmap="cool_r")
+        ax[i // 4, i % 4].set_title(title)
+        ax[i // 4, i % 4].axis("off")
+plt.show()
+
 
